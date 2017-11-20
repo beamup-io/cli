b168eecacdccbeb4c21d4beb0f6d8cee5e8a6630
Albert Zak <me@albertzak.com>
2017-11-20 14:52:59 +0100

    Compile and cache builder dependencies

--- priv/entrypoint.sh
+++ priv/entrypoint.sh
@@ -3,29 +3,37 @@
 set -e
 
 # Let Erlang know where to find modules
-cp /host/priv/.erlang ~/.erlang
+cp /host/builder/priv/.erlang ~/.erlang
 
 # Fetch CLI command scripts from the mounted volume
-mkdir -p /beamup/commands/
-cp -R /host/commands/* /beamup/commands/
+mkdir -p /beamup/builder/commands/
+cp -R /host/builder/commands/* /beamup/builder/commands/
 
 # Fetch and compile modules
-mkdir -p /beamup/src/
-cp -R /host/src/* /beamup/src/
-erlc -o /beamup/src/ /beamup/src/*.erl
+mkdir -p /beamup/builder/src/
+cp -R /host/builder/src/* /beamup/builder/src/
+erlc -o /beamup/builder/src/ /beamup/builder/src/*.erl
+
+# Compile builder
+cp /host/builder/rebar.config /beamup/builder/rebar.config
+cd /beamup/builder
+rebar3 compile
+
+# Write paths of compiled dependencies
+# to be read and loaded by ~/.erlang
+rebar3 path > paths
 
 if [[ ! "$@" = "selftest" ]]; then
   echo "Syncing working tree to scratch location"
   mkdir -p /beamup/project
-  cp -R /host/project/${PROJECT_NAME} /beamup/project/
-
-  cd /beamup/project/${PROJECT_NAME}
+  cp -R /host/project/${BEAMUP_PROJECT_NAME} /beamup/project/
+  cd /beamup/project/${BEAMUP_PROJECT_NAME}
 fi
 
 # Enable shell history in OTP 20+
 export ERL_AFLAGS="-kernel shell_history enabled"
 
 # The CLI commands are executable escripts
-export PATH="/beamup/commands:$PATH"
+export PATH="/beamup/builder/commands:$PATH"
 
 exec $@
