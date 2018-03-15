a107a122b8025ff33d8fe808837eaa840d84f9bd
Albert Zak <me@albertzak.com>
2018-03-15 13:41:08 +0100

    Boot release

--- src/beamup_node.erl
+++ src/beamup_node.erl
@@ -13,13 +13,48 @@ run(Name, WantedBranch, Url, Secret) ->
   Project = beamup_project:new_remote(Name, Branch),
   StoredVersions = beamup_store:versions(Store, Project),
   LatestVersion = get_latest_version(Project, StoredVersions),
+  io:format("Downloading~n"),
   TarPath = beamup_store:get(Store, Project, LatestVersion),
   beamup_fileutil:copy(TarPath, ReleasesDir),
+  io:format("Extracting~n"),
   beamup_fileutil:extract(TarPath, ActiveDir),
-  beamup_fileutil:remove(TarPath).
+  beamup_fileutil:remove(TarPath),
+  ok = filelib:ensure_dir(<<ActiveDir/binary, "/bin">>),
+  io:format("Booting~n"),
 
+  Port = open_port({spawn_executable, os:find_executable(binary_to_list(<<ActiveDir/binary, "/bin/", Name/binary>>))},
+                   [stream, eof, exit_status,
+                    {cd, <<ActiveDir/binary, "/bin/">>},
+                    {args, ["foreground"]}]),
+  stream(Port, fun(Bytes) -> io:put_chars(Bytes) end).
 
 % Private
+
+stream(Port, Handler) ->
+  receive
+    {Port, {data, Bytes}} ->
+      Binary = list_to_binary(Bytes),
+      Handler(Binary),
+      stream(Port, Handler);
+    {Port, eof} ->
+      Port ! {self(), close},
+      receive
+        {Port, closed} ->
+          true
+      end,
+      receive
+        {'EXIT', Port, _} ->
+          ok
+      after 1 ->
+        ok
+      end,
+      ExitCode =
+        receive
+          {Port, {exit_status, Code}} ->
+            Code
+      end,
+      {ExitCode}
+  end.
 
 default_branch(<<>>) ->
   io:format("Warning: Falling back to default branch `master`~n"),
